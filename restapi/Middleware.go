4f9dbe764040a6ff4eedaff715651bebe5364ebc
vovsike <vladimir.garanin@stfc.ac.uk>
2025-06-08 19:42:38 +0100

    Remove dead code

--- restapi/Middleware.go
+++ restapi/Middleware.go
@@ -11,18 +11,3 @@ func Logging(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
-
-//func Auth(next http.Handler) http.Handler {
-//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-//		token := r.Header.Get("Authorization")
-//		jwtValue := strings.Split(token, "Bearer")[1]
-//		priv, _ := loadRSAPrivateKeyFromFile("private.pem")
-//		_, err := jwt.Parse([]byte(jwtValue), jwt.WithKey(jwa.RS256(), priv.Public()))
-//		if err != nil {
-//			log.Println("JWT validation failed, error: ", err, "")
-//			w.WriteHeader(http.StatusUnauthorized)
-//			return
-//		}
-//		next.ServeHTTP(w, r)
-//	})
-//}
